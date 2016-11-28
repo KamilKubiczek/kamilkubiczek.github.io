fb1e6d9c967896b944566c6232bab6a98939ed3c
KamilKubiczek <kamilkubiczek1@gmail.com>
2016-11-28 18:37:28 +0100

    validator

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-	/*var x;
-$(window).on('scroll', function() {
-    x = $(window).scrollTop();
-    $('#header').css('background-size', 100 + parseInt(x / 10, 0) + '% ');
-});*/
-
 
 
 
@@ -14,21 +8,13 @@ var width = $(window).width();
 function star() {
 if ( $(window).width() > 550) {
  $('#sstar').animate({
-                    //left: '-120px',
-                    //top: '500px',
                     left: '-5vw',
                     top: '70vh',
                     opacity: '1',
                     reset: true,
                     delay: 600
-                   // height: '150px',
-                    //width: '150px'
                 }, 1000, function () { $(this).removeAttr('style'); });
-
-
-
 }
-
 };
 
 setInterval(function() {
@@ -39,35 +25,28 @@ setInterval(function() {
 
 
 
-
-
-
-
 // MENU SLIDE DOWN
 
  
-         $('#port').find('a').on('click',function(e){
-            if ( $(window).width() < 550) {
+$('#port').find('a').on('click',function(e){
+    if ( $(window).width() < 550) {
        $(this).removeAttr("href");
        $(this).attr("href", "#portfolio2");
-        }
-    })
+    }
+})
                
       
 
-    var menu = $('.menu');
-    var bottom = $('.bottom_big');
-    var bottom2 = $('.bottom_big_2');
-    var bottomUp = $('.bottom_big_up');
+var menu = $('.menu');
+var bottom = $('.bottom_big');
+var bottom2 = $('.bottom_big_2');
+var bottomUp = $('.bottom_big_up');
 
 
     menu.find('a').on('click',function(e){
-        //console.log('działa');
         e.preventDefault();
         var href = $(this).attr('href');
-        console.log(href);
         var position = $(href).offset();
-        console.log(position.top);
 
         $('html,body').animate({scrollTop: position.top}, 1500);
     })
@@ -75,37 +54,31 @@ setInterval(function() {
          
 
     bottom.find('a').on('click',function(e){
-        //console.log('działa');
         e.preventDefault();
         var href = $(this).attr('href');
-        console.log(href);
         var position = $(href).offset();
-        console.log(position.top);
 
         $('html,body').animate({scrollTop: position.top}, 1500);
     })
 
     bottom2.find('a').on('click',function(e){
-        //console.log('działa');
         e.preventDefault();
         var href = $(this).attr('href');
-        console.log(href);
         var position = $(href).offset();
-        console.log(position.top);
 
         $('html,body').animate({scrollTop: position.top}, 1500);
     })
 
     bottomUp.find('a').on('click',function(e){
-        //console.log('działa');
         e.preventDefault();
         var href = $(this).attr('href');
-        console.log(href);
         var position = $(href).offset();
-        console.log(position.top);
 
         $('html,body').animate({scrollTop: position.top}, 1500);
     })
+
+
+
 
 
 // SKRYPT POJAWIAJĄCY SIĘ
@@ -113,11 +86,11 @@ setInterval(function() {
 window.sr = ScrollReveal();
 var logo = document.querySelector('#section_logo');
 var skills = document.querySelectorAll('#skills');
-// var img = skills.querySelectorAll('img');   
+
 	sr.reveal('h1', {
         reset: true,
         delay: 200,
-        easing   : 'ease-in-out',
+        easing: 'ease-in-out',
         scale: 1,
         origin: 'bottom',
     });
@@ -125,7 +98,7 @@ var skills = document.querySelectorAll('#skills');
 	sr.reveal('p', {
         reset: true,
         delay: 400,
-        easing   : 'ease-in-out',
+        easing: 'ease-in-out',
         scale: 1,
         origin: 'bottom',
     });
@@ -133,7 +106,7 @@ var skills = document.querySelectorAll('#skills');
     sr.reveal('hr', {
         reset: true,
         delay: 600,
-        easing   : 'ease-in-out',
+        easing: 'ease-in-out',
         scale: 1,
         origin: 'left',
     });
@@ -141,7 +114,6 @@ var skills = document.querySelectorAll('#skills');
     sr.reveal(logo, {
         reset: true,
         delay: 800,
-      //  easing   : 'ease-in-out',
         distance: '0',
         scale: 1,
         origin: 'right',
@@ -149,7 +121,6 @@ var skills = document.querySelectorAll('#skills');
     });
 
 $(function(){
-    //console.log('DOM!');
     $(document).ready(function(){
 	$('#nav-icon3').click(function(){
 		$(this).toggleClass('open');
@@ -161,11 +132,9 @@ $(function(){
 
     function slider() {
         
-        //nasze przyciski
         var next = $('#nextPicture');
         var prev = $('#prevPicture');
         
-        //wyszukane elementy 
         var slider = $('#slider');
         var ul = slider.find('ul')
         var li = slider.find('li');
@@ -178,7 +147,6 @@ $(function(){
         
         ul.append(first).prepend(last);
         ul.css('left', -widthLi);
-        //console.log(li.length);
         
         ul.width((li.length)*widthLi);
         
@@ -197,7 +165,7 @@ $(function(){
             ul.animate({left: -(widthLi*index)}, function(){
                 console.log(li.length, index);
                 if(index < 1 ) {
-                    index = 2; // tu miales niepotrzebnie 4
+                    index = 2; 
                     ul.css('left', -index*widthLi);
                 }
             });
@@ -206,134 +174,68 @@ $(function(){
     
     slider();
 
-// SKILLS 
-/*
-var skills = $('.skills');
-var img = skills.find('img');
-skills.on('mouseover', function(){
-    $(this).find('img').css('transform' ,'scale(1.3)');
-    $(this).find('img').css('opacity','.2');
-    $(this).animate({ 
-        background: 'black',
-        opacity: '0.7',    
-    },100);
-   $(this).css('background','black');
-   $(this).text('40%');
-})
-skills.on('mouseout', function(){
-    $(this).find('img').css('transform' ,'scale(1)');
-    $(this).find('img').css('opacity' ,'1');
-    $(this).css('opacity','1');
-    $(this).css('background','');
-    $(this).text('');
-})
-*/
-
-// SLIDER BLUR
-/*
-$('#portfolio').on('mouseenter', function(){
-
-  $(this).find('img').animate({
-            //left: '+=50px',
-            opacity: '.7',
-            transform: 'scale(1.2)'
-        }, 500)
-       // $(this).find('hr').css('')
-    
-})
-$('#portfolio').on('mouseleave', function(){
-
-  $(this).find('img').animate({
-            //left: '+=50px',
-            opacity: '1',
-            transform: 'scale(1.2)'
-        }, 300)
-       // $(this).find('hr').css('')
-    
-})*/
-
-
-
-
-
-
 
 
 //STICKY MENU
 
 
-//$(function(){
+
 var stickyNavTop = $('nav').offset().top;
- var btn = $('.btn-menu');
+var btn = $('.btn-menu');
 var menu2 = $('.menu2');
 var menu = $('.menu');
 
 var stickyNav = function(){
 var scrollTop = $(window).scrollTop();
 var width = $(window).width();
-//var pix = 550px; 
+
       
 if (scrollTop > stickyNavTop && width > 550 && !btn.hasClass('open')) { 
-   // console.log('600')
+
     btn.css('position','fixed');
     btn.removeClass('hidden-btn');
     btn.css('display','block');
     menu2.addClass('hidden-nav');
-    //btn.css("margin","0");
-    
     btn.css("right","4vw");
     btn.css("margin-top","0");
-    //menu.find('a').css('font-size','5vh');
 
 } else if (width < 550 && !btn.hasClass('open')) { 
-  //console.log('500')
-   // $('.title').find('h2').css('font-size', '12vh');
+
     btn.css('position','fixed');
     btn.removeClass('hidden-btn');
     menu2.addClass('hidden-nav');
     btn.css("margin-right","20px");
+
 } else if (btn.hasClass('open')) { 
+
   console.log('open');
-  //menu.css('font-family','gt_walsheim_proultra_light');
     
 }else if (scrollTop > stickyNavTop) { 
   
     btn.css("padding-right","20px");
     btn.css("right","120px");
+
 }else {
     btn.removeClass('sticky');
     btn.css('position','');
     btn.addClass('hidden-btn');
     btn.css('display','none');
     menu2.removeClass('hidden-nav');
-    //menu.find('a').css('font-size','4vh');
+
 }
-
-
 };
  
-
 stickyNav();
  
 $(window).scroll(function() {
   stickyNav();
 });
-/*
-$('.menu').on('click',function(){
-    btn.removeClass('open');
-    $('ul').removeClass('sidenav');
-    menu2.addClass('hidden-nav');
-})*/
-
-//})
-
 
 
 // PODWÓJNE KLIKANIE IPHONE
 
 $('.menu').find('a').on('click touchend', function(e) {
     
-   // window.location = link;
     btn.removeClass('open');
     $('ul').removeClass('sidenav');
     menu2.addClass('hidden-nav');
@@ -377,41 +279,22 @@ var menu = $('.menu').find('a');
 
  menu.hover(function(){
         $(this).find('hr').animate({
-            //left: '+=50px',
             float: 'right',
             width: '100%'
         }, 500).css('float','left')
-       // $(this).find('hr').css('')
     }, function(){
         $(this).find('hr').stop().animate({
-           // left: '+=50px' ,
-          // float: 'right',
             width: '-0'
         }, 300).css('float','right')
-       // $(this).find('hr').stop().animate({ "left": "+=50px" }, "slow" );
   });
 
 
-/* menu.on('mouseenter',function(){
-        $(this).find('hr').animate({
-            left: '+=50',
-            width: '100%'
-        }, 500)
-  });
-  menu.on('mouseleave',function(){
-        $(this).find('hr').animate({
-            right: '-50px',
-            width: '0%'
-        }, 500)
-  });*/
 
 
 
 
 // PARALLAX 
 var width = $(window).width();
-
-
 
 $(document).ready(function() {
     
@@ -420,9 +303,7 @@ var height = movementStrength / $(window).height();
 var width = movementStrength / $(window).width();
 if ( $(window).width() > 550   && $(window).width() < 1500) {
 $("#header").mousemove(function(e){
-           // $('#header').animate({
-           //     transform: 'scale(1.1)'
-           // },1000);
+
           var pageX = e.pageX - ($(window).width() );
           var pageY = e.pageY - ($(window).height() );
           var newvalueX = width * pageX * -1 - 25;
@@ -437,16 +318,6 @@ if ($(window).width() > 1500) {
 };
 
 
-/*
-    var h1 = $('h1');
-    var hr_about = $('#section_text_inside').find('hr');
-    h1.on('mouseover', function(){
-    	hr_about.css('display', 'block');
-    })
-
-$('#section_logo').find('img').show();
-
-*/
 });
 	
 
@@ -465,7 +336,6 @@ function _btnMenuInit() {
          var sidenav = document.querySelector('.sidenav');
 
         btn.addEventListener('click', function(){
-           //console.log('działa');
            nav.classList.toggle('hidden-nav');
            navUl.classList.toggle('sidenav');
 
@@ -547,12 +417,6 @@ function test_match_media_with_listener2(){
     }
 }
 
-
-
-
-
-    
-     //test_match_media_with_listener2();
 
 
 
